5503ee1f4ff63c8bea907dd2954b80568b3d5c42
kaio-w-b <141793140+kaio-w-b@users.noreply.github.com>
2025-05-21 20:55:15 -0300

    Update Gorgeta.py

--- Gorgeta.py
+++ Gorgeta.py
@@ -1,3 +1,19 @@
 def gorjeta(valor_conta: float, porcentagem_gorjeta: float) -> float:
 
     return valor_conta * (porcentagem_gorjeta / 100)
+
+
+
+try:
+        
+        valor_conta = float(input("Informe o valor da conta: R$ "))
+        percentual_gorjeta = float(input("Informe o percentual da gorjeta (%): "))
+        
+        valor_gorjeta = gorjeta(valor_conta, percentual_gorjeta)
+        total = valor_conta + valor_gorjeta
+        
+        print(f"\nValor da gorjeta: R$ {valor_gorjeta:.2f}")
+        print(f"Valor total (conta + gorjeta): R$ {total:.2f}")
+
+except ValueError:
+    print("Por favor, insira valores numéricos válidos.")
