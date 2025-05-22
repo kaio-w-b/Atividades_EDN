cb13cd740d4aff1dd0f6bb8070233ec5f9bddf79
kaio-w-b <141793140+kaio-w-b@users.noreply.github.com>
2025-05-22 20:52:36 -0300

    Create Saidas.py

---
+++ Atividade 3/Saidas.py
@@ -0,0 +1,11 @@
+from meu_pacote import mensagens, operacoes, matematica
+
+print(mensagens.boas_vindas("Kaio"))
+print(operacoes.multiplica(4, 5))
+
+
+print(matematica.soma(2, 5))
+print(matematica.subtrai(10, 4))
+print(matematica.modulo(5))
+print(operacoes.multiplica(3,7))
+
